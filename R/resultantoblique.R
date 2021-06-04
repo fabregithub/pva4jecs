0b10e35d22544a2be00333afaf7b0af81d4278ff
Shoji F. Nakayama <fabre@nies.go.jp>
2021-06-04 16:21:05 +0900

    Bug fix, 2021-06-04

--- R/resultantoblique.R
+++ R/resultantoblique.R
@@ -8,15 +8,18 @@
 #' @param y A11
 #' @param z O0
 #' @param X data
+#' @param ncols Number of columns in the original matrix X
+#' @param nrows Number of rows in the original matrix X
+#' @param k Number of end-members
 #'
 #' @export
 #'
 
-resultant_oblique <- function(x, y, z, X) { # Use oblique reference axes to get the resultant oblique loadings and scores
+resultant_oblique <- function(x, y, z, X, ncols, nrows, k) { # Use oblique reference axes to get the resultant oblique loadings and scores
   F011 <- z %*% y
   O01<- solve(z)
   A011 <- x %*% O01
-  SB <- scale_back(A011, F011, X)
+  SB <- scale_back(A011, F011, X, ncols = ncols, nrows = nrows, k = k)
   res <- list(A0 = SB$A0, F0 = SB$F0)
   return(res)
 }
