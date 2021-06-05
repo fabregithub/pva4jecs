69c834d982e575e57049721dc6ac70e21276ca93
Shoji F. Nakayama <fabre@nies.go.jp>
2021-06-05 15:33:40 +0900

    Adding sample data

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Matrix A
+#'
+#' Matrix A is original data from the book to create the dataset X
+#'
+'orgA0'
+
+#' Matrix B
+#'
+#' Matrix F is original data from the book to create the dataset X
+#'
+'orgF0'
+
+#' Dataset X
+#'
+#' Dataset X is generated from Matrix A and F
+#'
+'dataX'
