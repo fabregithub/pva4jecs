5fd9dbb4c990ee289c155e91663b4a6474d04aee
Shoji F. Nakayama <fabre@nies.go.jp>
2021-06-07 08:28:23 +0900

    Bug fix

--- R/pva.R
+++ R/pva.R
@@ -32,7 +32,7 @@ PVA <- function(X, k, N = 10) {
   n <- 0
   while (n < N){
     A111 <- Varimax(A11, gamma = 1.0, q = 20, tol = 1e-6)
-    O0 <- A_O(A111)
+    O0 <- A_O(A111, k)
     ro <- resultant_oblique(A111, F11, O0, X111)
     A0 <- ro$A0
     F0 <- ro$F0
